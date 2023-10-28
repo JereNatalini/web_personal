691c7b076c2249ab84019104feb4ce11e14b3e4e
Naranjax <jerenatalini36@gmail.com>
2023-10-28 15:32:00 -0300

    Activida encuesta V2

---
+++ actividad_encuestas_v2/src/componentes/Menu.jsx
@@ -0,0 +1,21 @@
+//Enrutamiento
+
+import React from "react";
+import {Link} from 'react-router-dom'
+
+const Menu = () => {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                    <Link to="/encuesta/crear">Crear encuesta</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Menu;
